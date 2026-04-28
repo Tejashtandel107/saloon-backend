bbcef149cd0fe969f08e55fbcdec8c36069402f0
tejashtandel <tejas@itidoltechnologies.com>
2026-04-28 12:11:16 +0530

    display faq in dashboard

--- src/modules/dashboard/service.ts
+++ src/modules/dashboard/service.ts
@@ -2,16 +2,18 @@ export  {};
 const Contact = require("../contact/schema");
 const Booking = require("../booking/schema");
 const Service = require("../service/schema");
+const Faq = require("../faq/schema");
 
 class DashboardService {
   async dashboardStats() {
-    const [contacts, bookings, services] = await Promise.all([
+    const [contacts, bookings, services, faqs] = await Promise.all([
       Contact.countDocuments(),
       Booking.countDocuments(),
       Service.countDocuments(),
+      Faq.countDocuments()
     ]);
 
-    return {contacts,bookings,services};
+    return {contacts,bookings,services,faqs};
   }
 }
 
